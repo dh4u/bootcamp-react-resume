542fd12d0778556911c7a2aa0e0891cf1140551c
scott harvey <donzail@hotmail.com>
2019-08-28 13:58:11 -0400

    Updated the "Works" section
    Still need to add CLR work

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
+var linkify = require('linkifyjs');
+var linkifyHtml = require('linkifyjs/html');
 
 class Portfolio extends Component {
+  linkify = this.linkify
   render() {
 
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
+      var projectImage = projects.image
+      var projectDescription = linkifyHtml(projects.description)
+
         return <div key={projects.title} className="columns portfolio-item">
            <div className="row">
            <div className="item-wrap">
             <a href={projects.url} title={projects.title} target="_blank">
-               <img alt={projects.title} src={projectImage} />
+              <img alt={projects.title} src={process.env.PUBLIC_URL + projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
                   <h5>{projects.title}</h5>
+                     {/* <p dangerouslySetInnerHTML={ {__html: projects.category} }/> */}
                      <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
           <div>
           <h5>{projects.title}</h5>
-          <p>{projects.description}</p>
+          {/* the description was not converting the text to clickable hyperlinks */}
+          {/* <p>{projects.description}</p> */}
+          {<p dangerouslySetInnerHTML={ {__html: projectDescription} } />}
           { projects.technology != "" ?
             (
-              <p>Concepts / technologies included:<br/>{projects.technology}</p>
+              <p><strong><i>Concepts / technologies included:</i></strong><br/>{projects.technology}</p>
             )
           :
             ("")
