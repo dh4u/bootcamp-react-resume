aad200baeebeea356502d01f76797b319b2d6201
scott harvey <donzail@hotmail.com>
2019-09-06 06:52:16 -0400

    Footer Updates
    Attributed resume code to Tim Baker and removed bullets from footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,6 +21,7 @@ class Footer extends Component {
            <ul className="copyright">
               <li>&copy; Copyright 2019 Scott Harvey</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>This resume is a fork of Tim Baker's ReactJs template - <a href="https://github.com/tbakerx/react-resume-template">https://github.com/tbakerx/react-resume-template</a></li>
            </ul>
 
         </div>
