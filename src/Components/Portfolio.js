5ce1e0ecb97032e568863702a960a7d1c7afd82f
scott harvey <donzail@hotmail.com>
2019-09-09 00:55:10 -0400

    Update Portfolio.js
    Added captions to lightbox images

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -66,41 +66,41 @@ class Portfolio extends Component {
 							<hr />
 							<div style={{display: 'flex', flexWrap: 'wrap'}}>
 								<div key="CLR - Dashboard" className="portfolio-item column" style={{padding: '0 20px', boxSizing: 'border-box'}}>
-									<a href="/images/portfolio/clr-dashboard.cropped.png" alt="CLR Dashboard" data-lightbox="CLR">
-										<img src={'/images/portfolio/thumbnails/clr-dashboard.thumbnail.png'} alt="CLR Dashboard" />
+									<a href="/images/portfolio/clr-dashboard.cropped.png" alt="CLR Dashboard" data-lightbox="CLR" data-title="The home page for The Eye" >
+										<img src={'/images/portfolio/thumbnails/clr-dashboard.thumbnail.png'} alt="CLR Dashboard"/>
 									</a>
 								</div>
 								<div key="CLR - Timesheet" className="portfolio-item column" style={{padding: '0 20px', boxSizing: 'border-box'}}>
-									<a href="/images/portfolio/clr-timesheet.form.cropped.png" alt="CLR Timesheet" data-lightbox="CLR">
+									<a href="/images/portfolio/clr-timesheet.form.cropped.png" alt="CLR Timesheet" data-lightbox="CLR" data-title="Timesheet entry form">
 										<img src={'/images/portfolio/thumbnails/clr-timesheet.form.thumbnail.png'} alt="CLR Timesheet" />
 									</a>
 								</div>
 								<div key="CLR - IntranetCMS - HR" className="portfolio-item column" style={{padding: '0 20px', boxSizing: 'border-box'}}>
-									<a href="/images/portfolio/clr-intranetCMS.hr.cropped.png" alt="CLR IntranetCMS HR" data-lightbox="CLR">
+									<a href="/images/portfolio/clr-intranetCMS.hr.cropped.png" alt="CLR IntranetCMS HR" data-lightbox="CLR" data-title="Content Management for HR documents and policies">
 										<img src={'/images/portfolio/thumbnails/clr-intranetCMS.hr.thumbnail.png'} alt="CLR IntranetCMS - HR" />
 									</a>
 								</div>
 								<div key="CLR - IntranetCMS - Supervisor Resources" className="portfolio-item column" style={{padding: '0 20px', boxSizing: 'border-box'}}>
-									<a href="/images/portfolio/clr-intranetCMS.supervisor.resources.cropped.png" alt="CLR IntranetCMS Supervisor Resources" data-lightbox="CLR">
+									<a href="/images/portfolio/clr-intranetCMS.supervisor.resources.cropped.png" alt="CLR IntranetCMS Supervisor Resources" data-lightbox="CLR" data-title="Content Management of resources for supervisors">
 										<img src={'/images/portfolio/thumbnails/clr-intranetCMS.supervisor.resources.thumbnail.png'} alt="CLR IntranetCMS - Supervisor Resources" />
 									</a>
 								</div>
-								<a href="/images/portfolio/clr-scheduled.task.manager.cropped.png" alt="CLR Scheduled Task Manager" data-lightbox="CLR" style={{display: 'none'}}>
+								<a href="/images/portfolio/clr-scheduled.task.manager.cropped.png" alt="CLR Scheduled Task Manager" data-lightbox="CLR" data-title="Scheduled task manager" style={{display: 'none'}}>
 									<img src={'/images/portfolio/thumbnails/clr-scheduled.task.manager.thumbnail.png'} alt="CLR IntranetCMS - Scheduled Task Manager" />
 								</a>
-								<a href="/images/portfolio/clr-intranetCMS.intranet.form.cropped.png" alt="CLR IntranetCMS Content Management" data-lightbox="CLR" style={{display: 'none'}}>
+								<a href="/images/portfolio/clr-intranetCMS.intranet.form.cropped.png" alt="CLR IntranetCMS Content Management" data-lightbox="CLR" data-title="Content management for intranet content" style={{display: 'none'}}>
 									<img src={'/images/portfolio/thumbnails/clr-intranetCMS.intranet.form.thumbnail.png'} alt="CLR IntranetCMS - Content Management" />
 								</a>
-								<a href="/images/portfolio/clr-expense.reports.home.cropped.png" alt="CLR Expense Reports" data-lightbox="CLR" style={{display: 'none'}}>
+								<a href="/images/portfolio/clr-expense.reports.home.cropped.png" alt="CLR Expense Reports" data-lightbox="CLR" data-title="Expense reporting" style={{display: 'none'}}>
 									<img src={'/images/portfolio/thumbnails/clr-expense.reports.home.thumbnail.png'} alt="CLR Expense Reports" />
 								</a>
-								<a href="/images/portfolio/clr-employee.data.report.cropped.png" alt="CLR Employee Data Report" data-lightbox="CLR" style={{display: 'none'}}>
+								<a href="/images/portfolio/clr-employee.data.report.cropped.png" alt="CLR Employee Data Report" data-lightbox="CLR" data-title="A sample employee data report for annual review dates" style={{display: 'none'}}>
 									<img src={'/images/portfolio/thumbnails/clr-employee.data.report.thumbnail.png'} alt="CLR Employee Data Report" />
 								</a>
 								{/* <a href="/images/portfolio/clr-staff.meeting.admin.home.cropped.png" alt="CLR Staff Meeting Admin" data-lightbox="CLR" style={{display: 'none'}}>
-									<img src={'/images/portfolio/thumbnails/clr-staff.meeting.admin.home.thumbnail.png'} alt="CLR Staff Meeting Admin" />
+									<img src={'/images/portfolio/thumbnails/clr-staff.meeting.admin.home.thumbnail.png'} alt="CLR Staff Meeting Admin" data-title="Content management tool for staff meetings" />
 								</a> */}
-								<a href="/images/portfolio/clr-org.chart.cropped.png" alt="CLR Org Chart" data-lightbox="CLR" style={{display: 'none'}}>
+								<a href="/images/portfolio/clr-org.chart.cropped.png" alt="CLR Org Chart" data-lightbox="CLR" data-title="A dynamic org chart" style={{display: 'none'}}>
 									<img src={'/images/portfolio/thumbnails/clr-org.chart.thumbnail.png'} alt="CLR Org Chart" />
 								</a>
 							</div>
