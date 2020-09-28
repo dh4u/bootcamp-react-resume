acb82cc115ba0bb701cd80abe0b7743314f06723
scott harvey <donzail@hotmail.com>
2020-09-27 22:11:40 -0400

    Update App.js
    Hid portfolio section

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.resume}/>
+        {/* <Portfolio data={this.state.resumeData.resume}/> */}
         {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
